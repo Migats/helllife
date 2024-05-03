b6e13176ecabf853e61be6619aec7c905f3ba028
migat <migats21@outlook.com>
2024-05-03 14:31:20 +0200

    Initial commit

---
+++ src/main/java/net/migats21/helllife/HellLife.java
@@ -0,0 +1,10 @@
+package net.migats21.helllife;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HellLife implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/migats21/helllife/client/HellLifeClient.java
@@ -0,0 +1,10 @@
+package net.migats21.helllife.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HellLifeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

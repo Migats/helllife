52b6a6c225b58dfc37400e9bf16028f72daad61f
migat <migats21@outlook.com>
2025-03-01 17:21:56 +0100

    Fixed item models

--- src/main/java/net/migats21/helllife/world/biome/GhostDomeCarver.java
+++ src/main/java/net/migats21/helllife/world/biome/GhostDomeCarver.java
@@ -24,29 +24,29 @@ public class GhostDomeCarver extends WorldCarver<CaveCarverConfiguration> {
     }
 
     @Override
-    public boolean carve(CarvingContext carvingContext, CaveCarverConfiguration carverConfiguration, ChunkAccess chunkAccess, Function<BlockPos, Holder<Biome>> function, RandomSource randomSource, Aquifer aquifer, ChunkPos chunkPos, CarvingMask carvingMask) {
+    public boolean carve(CarvingContext carvingContext, CaveCarverConfiguration carverConfiguration, ChunkAccess chunkAccess, Function<BlockPos, Holder<Biome>> biomeMap, RandomSource randomSource, Aquifer aquifer, ChunkPos chunkPos, CarvingMask carvingMask) {
         if (chunkPos.x != 0 && chunkPos.z != 0) return false;
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 35.0, 76.0, 6.0, 24, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 32.0, 64.0, 10.0, 24, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 32.0, 60.0, 11.0, 10, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 32.0, 56.0, 11.0, 20, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 32.0, 52.0, 11.0, 8, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 33.0, 48.0, 11.0, 16, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 33.0, 44.0, 12.0, 6, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 34.0, 40.0, 12.0, 12, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 35.0, 32.0, 13.0, 8, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 36.0, 24.0, 13.0, 4, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 37.0, 16.0, 14.0, 2, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 38.0, 6.0, 15.0, 2, carvingMask);
-        carveEllipsoid(carvingContext, carverConfiguration, chunkAccess, function, aquifer, 0.0, 44.0, 0.0, 20.0, 20.0, carvingMask, GhostDomeCarver::shouldSkip);
-        carveEllipsoid(carvingContext, carverConfiguration, chunkAccess, function, aquifer, 0.0, 72.0, 0.0, 20.0, 20.0, carvingMask, GhostDomeCarver::shouldSkip);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 76.0, 6.0, 15.0, 4, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 77.0, 16.0, 14.0, 4, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 78.0, 24.0, 13.0, 8, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 82.0, 32.0, 13.0, 8, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 86.0, 40.0, 12.0, 12, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 90.0, 44.0, 12.0, 6, carvingMask);
-        carveRing(carvingContext, carverConfiguration, chunkAccess, function, randomSource, aquifer, 92.0, 48.0, 11.0, 16, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 35.0, 76.0, 6.0, 24, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 32.0, 64.0, 10.0, 24, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 32.0, 60.0, 11.0, 10, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 32.0, 56.0, 11.0, 20, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 32.0, 52.0, 11.0, 8, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 33.0, 48.0, 11.0, 16, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 33.0, 44.0, 12.0, 6, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 34.0, 40.0, 12.0, 12, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 35.0, 32.0, 13.0, 8, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 36.0, 24.0, 13.0, 4, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 37.0, 16.0, 14.0, 2, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 38.0, 6.0, 15.0, 2, carvingMask);
+        carveEllipsoid(carvingContext, carverConfiguration, chunkAccess, biomeMap, aquifer, 0.0, 44.0, 0.0, 20.0, 20.0, carvingMask, GhostDomeCarver::shouldSkip);
+        carveEllipsoid(carvingContext, carverConfiguration, chunkAccess, biomeMap, aquifer, 0.0, 72.0, 0.0, 20.0, 20.0, carvingMask, GhostDomeCarver::shouldSkip);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 76.0, 6.0, 15.0, 4, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 77.0, 16.0, 14.0, 4, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 78.0, 24.0, 13.0, 8, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 82.0, 32.0, 13.0, 8, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 86.0, 40.0, 12.0, 12, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 90.0, 44.0, 12.0, 6, carvingMask);
+        carveRing(carvingContext, carverConfiguration, chunkAccess, biomeMap, randomSource, aquifer, 92.0, 48.0, 11.0, 16, carvingMask);
         return true;
     }
 
